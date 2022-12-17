a7146a6a1d45eff8d0f2298135d708fe65945b8f
12CUY <wiliampuntomorales45@gmail.com>
2022-12-17 17:37:57 -0500

    con

--- aplicacion/app.py
+++ aplicacion/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+# ruta pricicpal
+
+@app.route('/')
+def index():
+    return "hola mundo"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
