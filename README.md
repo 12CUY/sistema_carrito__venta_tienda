30e8418126b3a910d06edd43ea91d325f7fe12a1
12CUY <wiliampuntomorales45@gmail.com>
2023-01-09 09:01:24 -0500

    aplicacion_carpetas

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # sistema_carrito__venta_tienda
 
-
-   <p> Aplicacion para la compra de equipos de aire acondicionados  </p>
- 
-
 <h2>Todo los requerimentos esta --requerimientos.txt-- </h2>
--- aplicacion/app.py
+++ aplicacion/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from aplicacion import config
 
 app = Flask(__name__)
-# ruta pricicpal
+app.config.from_object(config)
+Bootstrap(app)
+db = SQLAlchemy(app)
+
 
 @app.route('/')
-def index():
-    return "hola mundo"
+def inicio():
+    return render_template("inicio.html")
 
 
-if __name__ == '__main__':
-    app.run(debug = True)
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error.html", error="Página no encontrada..."), 404
